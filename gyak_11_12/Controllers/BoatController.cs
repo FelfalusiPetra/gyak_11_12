ee4471dba281bc4f260acb3be0bc5f8709361027
Felfalusi Petra <felfalusip@gmail.com>
2023-11-29 16:40:51 +0100

    Add project files.

---
+++ gyak_11_12/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using gyak_11_12.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gyak_11_12.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("question/all")]
+        public IActionResult Mindegy()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
